2c2e5aa7e72046a22676a38add5c5e98cb88a2a6
Alan Casas <alancasasarevalo@gmail.com>
2017-11-15 21:04:43 +0100

    Menu created to get an Add button GridActivity modified to have - names as ArrayList and myAdapter as class property - Menu options added

--- app/src/main/java/com/example/alancasas/listviews/Activities/GridActivity.kt
+++ app/src/main/java/com/example/alancasas/listviews/Activities/GridActivity.kt
@@ -2,13 +2,20 @@ package com.example.alancasas.listviews.Activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.GridView
 import android.widget.Toast
 import com.example.alancasas.listviews.MyAdapter
 import com.example.alancasas.listviews.R
 
 class GridActivity : AppCompatActivity() {
-        private var gridView: GridView? = null
+    private var gridView: GridView? = null
+
+    private var counter:Int = 0
+
+    var names : ArrayList<String>? = null
+    var myAdapter : MyAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,30 +24,82 @@ class GridActivity : AppCompatActivity() {
 
         gridView = findViewById(R.id.grid_view)
 
-        var names = arrayListOf<String> (
+        names = arrayListOf<String> (
                 "Alan",
                 "Bibi",
                 "Vega",
                 "Mario",
                 "Elsa"
         )
-
-        names.add("Manuel")
-        names.add("Jesus")
-        names.add("Rodrigo")
-        names.add("Lusi")
-        names.add("Ej")
+        names!!.add("Jesus")
+        names!!.add("Rodrigo")
+        names!!.add("Lusi")
+        names!!.add("Ej")
 
         gridView?.setOnItemClickListener { adapterView, view, position, id ->
-            Toast.makeText(this, "Clicked " + names[position], Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Clicked " + names!![position], Toast.LENGTH_LONG).show()
         }
 
-        var myAdapter : MyAdapter = MyAdapter(this, R.layout.grid_item, names)
+        myAdapter = MyAdapter(this, R.layout.grid_item, names!!)
 
         gridView?.adapter = myAdapter
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var inflater = menuInflater
+        inflater.inflate(R.menu.action_bar_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+            R.id.add_item -> {
+                this.names?.add("Agregado n ${++counter}" )
+
+                this.myAdapter?.notifyDataSetChanged()
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
