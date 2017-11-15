fc8bd2cc0fdf27c9c490cd4b9eb45cd85bc90c0f
Alan Casas <alancasasarevalo@gmail.com>
2017-11-15 17:10:15 +0100

    Android Manifest fixed ListActivity modified to get an array adapter, and to add a string array to present into list view

--- app/src/main/java/com/example/alancasas/listviews/Activities/ListActivity.kt
+++ app/src/main/java/com/example/alancasas/listviews/Activities/ListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Adapter
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.alancasas.listviews.R
 
@@ -25,7 +26,26 @@ class ListActivity : AppCompatActivity() {
                 "Elsa"
         )
 
-        var adapter = Adapter()
+        names.add("Manuel")
+        names.add("Jesus")
+        names.add("Rodrigo")
+        names.add("Felipe")
+        names.add("Prdro")
+        names.add("Samuel")
+        names.add("Erika")
+        names.add("Julian")
+        names.add("Epistoles")
+        names.add("Ignacio")
+        names.add("Ruben")
+        names.add("Lusi")
+        names.add("Ej")
+
+
+
+
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names)
+
+        listView?.adapter = adapter
 
     }
 }
